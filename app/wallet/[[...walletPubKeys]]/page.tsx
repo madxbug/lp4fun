b870804e8736b6b48c416c9ebbabd80722a3d2ec
madxbug <segfaultx0@gmail.com>
2025-06-02 17:59:04 +0300

    Update all dependencies to latest versions (except Tailwind CSS)

--- app/wallet/[[...walletPubKeys]]/page.tsx
+++ app/wallet/[[...walletPubKeys]]/page.tsx
@@ -25,24 +25,25 @@ const createDataMap = async (wallet: string): Promise<Map<string, PoolData>> => 
     const map = new Map<string, PoolData>();
     await Promise.all(Array.from(positions.entries()).map(async ([key, position]) => {
         const tokenInfo = await fetchTokenPrice(position.tokenX.publicKey, position.tokenY.publicKey);
-
+        const tokenDecimalX = position.tokenX.mint.decimals;
+        const tokenDecimalY = position.tokenY.mint.decimals;
         const lbPairPositionsData = position.lbPairPositionsData.map((pos: LbPosition): PositionData => {
             const claimedFeeXAmount = pos.positionData.totalClaimedFeeXAmount.toString();
             const claimedFeeYAmount = pos.positionData.totalClaimedFeeYAmount.toString();
 
             return {
                 lastUpdatedAt: bnToDate(pos.positionData.lastUpdatedAt),
-                totalXAmount: formatTokenBalance(BigInt(pos.positionData.totalXAmount.split('.')[0]), position.tokenX.decimal),
-                totalYAmount: formatTokenBalance(BigInt(pos.positionData.totalYAmount.split('.')[0]), position.tokenY.decimal),
-                feeX: formatTokenBalance(BigInt(pos.positionData.feeX.toString()), position.tokenX.decimal),
-                feeY: formatTokenBalance(BigInt(pos.positionData.feeY.toString()), position.tokenY.decimal),
+                totalXAmount: formatTokenBalance(BigInt(pos.positionData.totalXAmount.split('.')[0]), tokenDecimalX),
+                totalYAmount: formatTokenBalance(BigInt(pos.positionData.totalYAmount.split('.')[0]), tokenDecimalY),
+                feeX: formatTokenBalance(BigInt(pos.positionData.feeX.toString()), tokenDecimalX),
+                feeY: formatTokenBalance(BigInt(pos.positionData.feeY.toString()), tokenDecimalY),
                 publicKey: pos.publicKey.toString(),
                 lowerBinId: pos.positionData.lowerBinId,
                 upperBinId: pos.positionData.upperBinId,
                 claimedFeeXAmount,
                 claimedFeeYAmount,
-                claimedFeeX: formatTokenBalance(BigInt(claimedFeeXAmount), position.tokenX.decimal),
-                claimedFeeY: formatTokenBalance(BigInt(claimedFeeYAmount), position.tokenY.decimal),
+                claimedFeeX: formatTokenBalance(BigInt(claimedFeeXAmount), tokenDecimalX),
+                claimedFeeY: formatTokenBalance(BigInt(claimedFeeYAmount), tokenDecimalY),
             };
         });
         const mintInfoX = await getTokenMetadata(connection, position.tokenX.publicKey);
@@ -53,8 +54,8 @@ const createDataMap = async (wallet: string): Promise<Map<string, PoolData>> => 
             nameY: mintInfoY?.symbol ?? 'Unknown Token Y',
             price: tokenInfo.price,
             activeBin: position.lbPair.activeId,
-            tokenXDecimal: position.tokenX.decimal,
-            tokenYDecimal: position.tokenY.decimal
+            tokenXDecimal: tokenDecimalX,
+            tokenYDecimal: tokenDecimalY
         });
     }));
 
